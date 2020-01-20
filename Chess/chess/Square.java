a356e4329b752a67c3eddb64fa7aaaa7a806b6f6
fatmemahmud <59645255+fatmemahmud@users.noreply.github.com>
2020-01-20 09:41:03 +0200

    Create Square.java

---
+++ Chess/chess/Square.java
@@ -0,0 +1,23 @@
+package chess;
+
+public abstract class Square {
+	protected String symbol;
+	public String color;
+	public String type;
+	
+	public Square(String typeIn){
+		type = typeIn;
+	}
+	
+	String getSymbol(){
+		return symbol;
+	}
+	public String getColor(){
+		return color;
+	}
+	public String getType(){
+		return type;
+	}
+
+	public abstract boolean checkMove(int[] moveFromReq, int[] moveToReq, String plyColor, boolean testKing);
+}
