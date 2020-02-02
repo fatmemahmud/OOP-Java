4f5009b24506ab06e621f508b6d7cc27857eea15
fatmemahmud <59645255+fatmemahmud@users.noreply.github.com>
2020-02-02 23:47:13 +0300

    Create Pice.java

---
+++ Tetris/Pice.java
@@ -0,0 +1,21 @@
+package TetrisPieces;
+
+import TetrisTypes.MoveType;
+
+public interface Piece {
+
+    public abstract void draw();
+
+    public abstract boolean isValidMove(MoveType mtype);
+
+    public abstract void clear();
+
+    public abstract void performMove(MoveType mtype);
+
+    public abstract void moveDown();
+
+    public abstract boolean canMove();
+
+    public abstract boolean contains(int i, int j);
+
+}
